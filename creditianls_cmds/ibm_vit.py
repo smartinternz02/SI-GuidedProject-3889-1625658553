0c5fd9d33f666bb376fb031589aa4fe11d92a18b
Vaikunth7 <vaikunthnarayan@gmail.com>
2021-07-19 01:55:38 +0530

    third commit

--- creditianls_cmds/ibm_vit.py
+++ creditianls_cmds/ibm_vit.py
@@ -14,18 +14,18 @@ myConfig = {
 
 def myCommandCallback(cmd):
     print("Message received from IBM IoT Platform: %s" % cmd.data['command'])
-    m=cmd.data['command']
-    
+    #m=cmd.data['command']
+   
 
 client = wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
 client.connect()
 
 while True:
-    water_level=random.randint(0,100)
-    light_intensity=random.randint(0,100)
-    myData={'water_level':water_level, 'light_intensity':light_intensity}
-    client.publishEvent(eventId="status", msgFormat="json", data=myData, qos=0, onPublish=None)
-    print("Published data Successfully: %s", myData)
+##    temp=random.randint(-20,125)
+##    hum=random.randint(0,100)
+##    myData={'temperature':temp, 'humidity':hum}
+##    client.publishEvent(eventId="status", msgFormat="json", data=myData, qos=0, onPublish=None)
+##    print("Published data Successfully: %s", myData)
     client.commandCallback = myCommandCallback
     time.sleep(2)
 client.disconnect()
